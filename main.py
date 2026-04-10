555e02ff285f47d9a109536bd8f79fbf2f34c9bf
Yuki <github@risaton.net>
2026-04-11 08:00:30 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import vrchatapi
+from vrchatapi import *
+from vrchatapi.api import *
+from vrchatapi.exceptions import *
+from vrchatapi.models import *
+
+def main():
+
+    pass
+
+if __name__ == '__main__':
+    main()
