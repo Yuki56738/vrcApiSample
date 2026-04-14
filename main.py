c0a0ed32325cfc7fad9edba656e21bdd679cd84e
Yuki <github@risaton.net>
2026-04-15 03:33:32 +0900

    refactor main.py and auth_to_vrc.py to improve user authentication display; update main function to show current user

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from vrchatapi import api_client
+
 from auth_to_vrc import *
 
 def main():
     print('Welcome to the UNDERGROUND...')
+    auth_api = loadCookieAndAuth()
+    current_user = auth_api.get_current_user()
+    print("Logged in as:", current_user.display_name)
 
 
 if __name__ == "__main__":
