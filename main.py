f4bae69c358294672316f923eda5e161c6a55293
Yuki <github@risaton.net>
2026-04-29 02:22:51 +0900

    refactor logging configuration; adjust urllib3 logging level to INFO and remove StreamHandler

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[
-                        logging.StreamHandler(),
                         logging.FileHandler('app.log'),
                     ])
-logging.getLogger('urllib3').setLevel(logging.DEBUG)
+logging.getLogger('urllib3').setLevel(logging.INFO)
 logging.getLogger('urllib3.connectionpool').setLevel(logging.DEBUG)
 logging.getLogger('requests').setLevel(logging.DEBUG)
 
